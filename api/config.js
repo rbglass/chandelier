a4f18e1502c4920f7f0018f5def546f03a945ec5
Anita Amini <baadge29@yahoo.co.uk>
2015-06-02 11:59:05 +0100

    config

--- api/config.js
+++ api/config.js
@@ -7,5 +7,12 @@ module.exports = {
     password     : process.env.PASSWORD     || require("./creds.json").bell.password,
     clientID     : process.env.CLIENTID     || require("./creds.json").bell.clientID,
     clientSecret : process.env.CLIENTSECRET || require("./creds.json").bell.clientSecret
-  }
+  },
+	database: {
+		dbname			: process.env.DB_NAME 			|| require("./creds.json").database.dbname,
+		dburl				: process.env.DB_URL 			|| require("./creds.json").database.dburl,
+		dbuser			: process.env.DB_USER 			|| require("./creds.json").database.dbuser,
+		dbpassword	: process.env.DB_PASSWORD	|| require("./creds.json").database.dbpassword,
+		sslmode			: process.env.SSLMODE 		|| require("./creds.json").database.sslmode
+	}
 };
