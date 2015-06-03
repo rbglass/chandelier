73c1421d17ebfb6a9b610a33c390bd9be5743e57
Anita Amini <baadge29@yahoo.co.uk>
2015-06-03 21:17:41 +0100

    setting the models

--- api/models/database.js
+++ api/models/database.js
@@ -22,12 +22,38 @@ var Users = sequelize.define("users", {
 });
 
 
-var Jobs = sequelize.define('jobs', {});
-var Job_items = sequelize.define('job_items', {});
+var Jobs = sequelize.define('jobs', {
+	job_id: 						Sequelize.STRING,
+	client: 						Sequelize.STRING,
+	project: 						Sequelize.STRING,
+	job_status: 				Sequelize.STRING,
+	order_type: 				Sequelize.STRING,
+	shipping_date: 			Sequelize.DATE,
+	num_of_job_items: 	Sequelize.INT,
+	parts_status: 			Sequelize.STRING,
+	last_update: 				Sequelize.DATE
+});
+
+
+var Job_items = sequelize.define('job_items', {
+	item_id: 			Sequelize.STRING,
+	product: 			Sequelize.STRING,
+	description: 	Sequelize.STRING,
+	glass: 				Sequelize.STRING,
+	metal: 				Sequelize.STRING,
+	flex: 				Sequelize.STRING,
+	bulb: 				Sequelize.STRING,
+	qty_req: 			Sequelize.INT,
+	qty_hot: 			Sequelize.INT,
+	qty_cold: 		Sequelize.INT,
+	qty_assem: 		Sequelize.INT,
+	qty_packed: 	Sequelize.INT,
+	notes: 				Sequelize.STRING
+});
 
 
 module.exports = {
-	Users: Users,
-	Jobs: Jobs,
-	Job_items: Job_items
+	Users: 			Users,
+	Jobs: 			Jobs,
+	Job_items: 	Job_items
 };
