6afca9861a0c223a6c65bda9656cb6704803be7b
James <james.griffiths.2222@googlemail.com>
2015-06-09 18:03:35 +0100

    tablerowtests

--- test/front/setup/utils.js
+++ test/front/setup/utils.js
@@ -24,3 +24,44 @@ export function withContainer(callback) {
 	callback(testElement);
 	React.unmountComponentAtNode(testElement);
 }
+
+
+export function stubRouterContext(Component, props, stubs) {
+
+	var React = require("react");
+  function RouterStub() { }
+
+  Object.assign(RouterStub, {
+    makePath () {},
+    makeHref () {},
+    transitionTo () {},
+    replaceWith () {},
+    goBack () {},
+    getCurrentPath () {},
+    getCurrentRoutes () {},
+    getCurrentPathname () {},
+    getCurrentParams () {},
+    getCurrentQuery () {},
+    isActive () {},
+    getRouteAtDepth() {},
+    setRouteComponentAtDepth() {}
+  }, stubs);
+
+  return React.createClass({
+    childContextTypes: {
+      router: React.PropTypes.func,
+      routeDepth: React.PropTypes.number
+    },
+
+    getChildContext () {
+      return {
+        router: RouterStub,
+        routeDepth: 0
+      };
+    },
+
+    render () {
+      return <Component {...props} />;
+    }
+  });
+}
