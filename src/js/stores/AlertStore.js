856e2eccff27519c27741379a98a98a351c3305a
James <james.griffiths.2222@googlemail.com>
2015-06-12 08:38:02 +0100

    alert

--- src/js/stores/AlertStore.js
+++ src/js/stores/AlertStore.js
@@ -18,15 +18,13 @@ const AlertStore = createStore({
 const onReceivingAction = action => {
 	let isServerAction = /RECEIVE/.test(action.type);
 
+	if (action.type === ActionTypes.RECEIVE_ALERT) {
+		alert = action.data;
+	}
 	if (isServerAction) {
-		if (action.type === ActionTypes.RECEIVE_ALERT) {
-			alert = action.data;
-		}
 		isLoading = false;
-		AlertStore.emitChange();
 	} else if (action.type === ActionTypes.IS_LOADING) {
 		isLoading = true;
-		AlertStore.emitChange();
 	}
 };
 
