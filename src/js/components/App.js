faf63e083698ee2ca58218df9ac9c9d3a1525248
James <james.griffiths.2222@googlemail.com>
2015-06-19 00:29:32 +0100

    forgot perf

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -2,10 +2,6 @@
 import React, { Component } from "react";
 import { RouteHandler } from "react-router";
 
-var Perf = window.Perf = require("react/addons").addons.Perf;
-
-Perf.start();
-
 export default class App extends Component {
 	render() {
 		return <RouteHandler />;
