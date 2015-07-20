48d8b1f1584b1956886207d25de2da4fcd389500
James <james.griffiths.2222@googlemail.com>
2015-07-20 19:44:38 +0100

    pdf and tags

--- test/src/utils/__isUsefulTag.js
+++ test/src/utils/__isUsefulTag.js
@@ -10,14 +10,15 @@ describe("isUsefulTag", () => {
 			"select": null,
 			"input": null,
 			"textarea": null,
-			"option": null
+			"option": null,
+			"button": null
 		});
 
 		const aFewUselessTags = keyMirror({
 			"div": null,
 			"form": null,
 			"h1": null
-		})
+		});
 
 		Object.keys(usefulTags).forEach(tag => {
 			assert(isUsefulTag(usefulTags[tag]));
